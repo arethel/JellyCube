2cdfb25c4844e802bdc9548fb044f8dd1b319f37
gigadev <gigadevteam666@gmail.com>
2022-12-26 11:07:49 +0300

    n

--- player.gd
+++ player.gd
@@ -1,13 +1,18 @@
 extends RigidBody2D
 
 
+
+var gravity_force = 200
+var jump_impulse = 700
+var sticking_force = 1000
+
 func _ready():
 #	applied_force = Vector2(0,98)
-	add_central_force(Vector2(0,98))
-	apply_central_impulse(Vector2(0,200))
+	add_central_force(Vector2(0,gravity_force))
 	
 	
 	
+
 
 
 var set_stickness = false
@@ -25,18 +30,33 @@ func _integrate_forces(state):
 	if(set_stickness):
 		if(state.get_contact_count()>0&&state.get_contact_local_normal(0)!=Vector2.ZERO):
 			var cont_norm = state.get_contact_local_normal(0)
-			applied_force=-cont_norm*300
+			$velocity.global_position=global_position-cont_norm*20
+			$velocity.global_rotation=cont_norm.angle()
+			$"../sticking_pos".global_position=global_position-cont_norm*20
+			
+			$"../sticking_joint".global_position=global_position
+			$"../sticking_joint".global_rotation=cont_norm.angle()+PI/2
+			$"../sticking_joint".rest_length=cont_norm.length()*10
+			$"../sticking_joint".length=cont_norm.length()*20
+			$"../sticking_joint".node_a=self.get_path()
+			$"../sticking_joint".node_b=state.get_contact_collider_object(0).get_path()
+			
+			
+			
+#			applied_force=-cont_norm*sticking_force
 		elif !area_of_stickness:
 			set_stickness=false
-			applied_force=Vector2(0,100)
+			applied_force=Vector2(0,gravity_force)
 #		linear_velocity=linear_velocity-cont_norm.tangent()*(cont_norm.tangent().dot(linear_velocity))
 		
 		
 	if jump:
-		jump = false
 		if state.get_contact_count()>0:
-			applied_force=Vector2(0,100)
-			apply_central_impulse(state.get_contact_local_normal(0)*300)
+			jump = false
+			$"../sticking_joint".node_a=""
+			applied_force=Vector2(0,gravity_force)
+			linear_velocity = state.get_contact_local_normal(0)*jump_impulse
+#			apply_central_impulse(state.get_contact_local_normal(0)*jump_impulse)
 	
 
 func needed_angel(ang1, ang2):
@@ -51,6 +71,7 @@ func needed_angel(ang1, ang2):
 func _on_player_body_entered(_body):
 	if !set_stickness:
 		applied_force = Vector2.ZERO
+		linear_velocity = Vector2.ZERO
 		set_stickness = true
 		area_of_stickness = true
 		sticking_body=_body
@@ -60,6 +81,7 @@ var press = false
 func _input(event):
 	if(event is InputEventMouseButton):
 		if event.button_index==1 and press!=event.pressed:
+			print(set_stickness)
 			if event.pressed:
 				press =true
 				set_stickness=false
